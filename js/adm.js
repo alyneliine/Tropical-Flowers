2bd25cb45ca009523bcda7b5a837ca0fa52099dd
Diego Borba <diegoborbas03@gmail.com>
2023-06-12 23:14:28 -0300

    update estrutura

--- js/adm.js
+++ js/adm.js
@@ -30,3 +30,13 @@
         }
     }
 })()
+
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+
+if (toastTrigger) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+  toastTrigger.addEventListener('click', () => {
+    toastBootstrap.show()
+  })
+}
